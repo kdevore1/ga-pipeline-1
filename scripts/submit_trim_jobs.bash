b656405651d22d4d3eae33aeef741da02b89a57a
Kira DeVore <kdevore1@asu.edu>
2022-03-29 15:18:00 -0700

    Add submission script

---
+++ scripts/submit_trim_jobs.bash
@@ -0,0 +1,10 @@
+# Variable Creation
+metadata=data/untrimmed_fastq/data_urls.txt
+outdir=data/trimmed_fastq
+infiles=$(cut -d' ' -f 1 $metadata | grep _1.fastq.gz)
+indir=data/untrimmed_fastq
+
+# for loop
+for filename in $infiles; do
+  sbatch scripts/trimmomatic.bash $indir/$filename $outdir
+done
