72a03372e65c426f8944f9c61aa82d05729956f5
Kira DeVore <kdevore1@asu.edu>
2022-04-01 09:41:24 -0700

    Add download reference genome script

--- scripts/download_ref_genome.bash
+++ scripts/download_ref_genome.bash
@@ -1,5 +1,5 @@
 # Setup variables
-ref_dir=~/dc_workshop/ga-pipline-1/data/ref_genome
+ref_dir=~/dc_workshop/ga-pipeline-1/data/ref_genome
 ref_file=$ref_dir/ecoli_rel606.fasta   
 url="ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/017/985/GCA_000017985.1_ASM1798v1/GCA_000017985.1_ASM1798v1_genomic.fna.gz"
 
